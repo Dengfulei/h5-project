fc494cadc0b298f84b1f77afd76071c4abd0b0f2
dengfulei <443756292@qq.com>
2025-05-16 17:29:46 +0800

    引入常用组件库

--- eslint.config.ts
+++ eslint.config.ts
@@ -21,7 +21,6 @@ export default defineConfigWithVueTs(
   skipFormatting,
   {
     rules: {
-      'prefer-const': 'error',
       'vue/multi-word-component-names': 'off', // 关闭该规则
     },
   },
---
+++ postcss.config.ts
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: {
+    // ...
+    'postcss-px-to-viewport': {
+      // options
+      unitToConvert: 'px',
+      // 设计稿的宽度
+      viewportWidth: 375,
+    },
+  },
+}
+
+/**
+ *  https://github.com/evrone/postcss-px-to-viewport/blob/master/README_CN.md
+ * 1 增加 postcss.config.ts
+ */
